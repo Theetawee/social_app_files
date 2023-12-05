fd69b460b0891343e34cf36d95d91d214990b6e1
Forever Inc dev <redodevs@gmail.com>
2023-12-05 09:40:00 +0300

    save

--- js/online.js
+++ js/online.js
@@ -5,7 +5,31 @@ if (navigator.onLine) {
   badge.style.display = "block";
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const NotificationBadge = document.getElementById("b-not");
 
+  if (
+    Notification.permission === "denied" ||
+    Notification.permission === "default"
+  ) {
+    if (NotificationBadge) {
+      NotificationBadge.style.display = "flex";
+    }
+  } else {
+    if (NotificationBadge) {
+      NotificationBadge.style.display = "none";
+    }
+
+  }
+});
+
+const msgBox = document.getElementById("webpush-message");
+
+if (msgBox) {
+  if (msgBox.textContent === "") {
+    msgBox.textContent = "Allow Waanverse to send notifications";
+  }
+}
 
 
 
